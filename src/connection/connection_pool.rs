883bd752d9645c770ef8bbbd296771be424d312c
Andrew Evans <asevans48@gmail.com>
2019-10-22 20:16:38 -0600

    working on pool ticket

--- src/connection/connection_pool.rs
+++ src/connection/connection_pool.rs
@@ -5,33 +5,92 @@ author Andrew Evans
 */
 
 use std::vec::Vec;
+use std::sync::{Arc, Mutex, RwLock};
 
-use crate::queue::amqp::AQMPConnectionInf;
+use crate::queue::amqp::AMQPConnectionInf;
 use crate::queue::rabbitmq_utils::QueueUtils;
+use std::borrow::Borrow;
 
 
+/// Structure storing the pool
 pub struct RabbitMQConnectionPool{
-    connections: Vec<QueueUtils>,
+    size: usisze,
+    connections: Vec<Arc<RwLock<QueueUtils>>>,
+    checked_out_connections: Vec<Arc::new(Mutex::new(QueueUtils))>,
 }
 
 
+/// Structure implementation
 impl RabbitMQConnectionPool{
 
-    fn add_connection(&self, conn_inf: AQMPConnectionInf){
-
+    /// Add a connection to the pool
+    fn add_connection(&self, conn_inf: AMQPConnectionInf) -> Result<(), &'static str>{
+        let protocol = conn_inf.get_protocol();
+        let host = conn_inf.get_host();
+        let port = conn_inf.get_port();
+        let vhost = conn_inf.get_vhost();
+        let username = conn_inf.get_username();
+        let password = conn_inf.get_password();
+        let is_ssl = conn_inf.get_ssl();
+        let r = QueueUtils::new(protocol, host, &port, vhost, username, password, is_ssl);
+        if(r.is_ok()){
+            let qutils = r.ok().unwrap();
+            Ok(())
+        }else{
+            Err("Failed to Establish Connection")
+        }
     }
 
+    /// Drop the connection
+    fn drop_connection(&mut self, idx: &usize){
+        if idx < self.connections.len() {
+            let mut new_vec = Vec::<Arc<RwLock<QueueUtils>>>::new();
+            for i in 0..self.connections.len() {
+                let qconn = self.connections.pop().unwrap();
+                if &i != idx {
+                    new_vec.push(qconn);
+                } else {
+                    let r = qconn.close_conn();
+                }
+            }
+            self.connections = new_vec;
+        }
+    }
+
+    /// Get the pool size
     fn get_pool_size(&self) -> usize{
-        let conns = self.connections;
-        9
+        self.connections.len()
     }
 
-    fn close(&self){
+    /// Close the connection pool
+    fn close(&mut self){
+        for i in 0..self.connections.len(){
+            let q = self.connections.pop();
+            q.unwrap().close_conn();
+        }
+    }
+
+    fn get_connection(&self){
 
     }
 
-    fn new(nconnections: &i8) -> RabbitMQConnectionPool{
+    fn release_connection(&self){
 
+    }
+
+    fn start(&self){
+
+    }
+
+    /// Create a new pool
+    fn new(size: usize, conn_inf: AMQPConnectionInf) -> RabbitMQConnectionPool{
+        let v = Vec::<Arc<RwLock<QueueUtils>>>::new();
+        let checked_out = Vec::<Arc<RwLock<QueueUtils>>>::new();
+        RabbitMQConnectionPool{
+            size: size,
+            connections: v,
+            checked_out_connections: checked_out,
+        }
     }
 }
 
@@ -52,6 +111,11 @@ mod tests{
 
     #[test]
     fn should_release_and_reset_dead_connection(){
+
+    }
+
+    #[test]
+    fn should_release_connection(){
 
     }
 
--- src/queue/amqp.rs
+++ src/queue/amqp.rs
@@ -5,7 +5,7 @@ Author Andrew Evans
 */
 
 
-pub struct AQMPConnectionInf{
+pub struct AMQPConnectionInf{
     protocol: String,
     host: String,
     port: i64,
@@ -13,4 +13,35 @@ pub struct AQMPConnectionInf{
     username: Option<String>,
     password: Option<String>,
     is_ssl: bool,
+}
+
+impl AMQPConnectionInf{
+
+    pub fn get_protocol(&self) -> String{
+        self.protocol.clone()
+    }
+
+    pub fn get_host(&self) -> String{
+        self.host.clone()
+    }
+
+    pub fn get_port(&self) -> i64{
+        self.port.clone()
+    }
+
+    pub fn get_vhost(&self) -> Option<String>{
+        self.vhost.clone()
+    }
+
+    pub fn get_username(&self) -> Option<String>{
+        self.username.clone()
+    }
+
+    pub fn get_password(&self) -> Option<String>{
+        self.password.clone()
+    }
+
+    pub fn get_ssl(&self) -> bool{
+        self.is_ssl.clone()
+    }
 }
